efdcf5a7c711f0007c0681090cc1d48627845f00
Amk Lass <lassanaa14@gmail.com>
2022-07-10 01:22:37 +0100

    fix linters

--- Api/rocketsApi.js
+++
@@ -1,9 +0,0 @@
-const url = 'https://api.spacexdata.com/v3/rockets';
-
-const getRocketApi = async () => {
-  const data = await fetch(`${url}`)
-    .then((response) => response.json());
-  return data;
-};
-
-export default getRocketApi;
